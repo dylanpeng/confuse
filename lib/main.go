d33fd18e795877a04aaec90f28988dd4c248a671
peng.chen <peng.chen@opay-inc.com>
2022-10-14 17:48:52 +0800

    20221014 add db

--- lib/main.go
+++ lib/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"confuse/common/entity"
+	"database/sql"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,7 +10,23 @@ import (
 
 func main() {
 	dsn := "dev:123!@#qweASD@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn2 := "dev:123!@#qweASD@tcp(127.0.0.1:3306)/test2?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	sqlDB, err := sql.Open("mysql", dsn)
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+
+	sqlDB2, err := sql.Open("mysql", dsn2)
+	db2, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: sqlDB2,
+	}), &gorm.Config{})
+
+	//db, err := gorm.Open(mysql.New(mysql.Config{
+	//	DriverName: "my_mysql_driver",
+	//	DSN:        dsn, // data source name, refer https://github.com/go-sql-driver/mysql#dsn-data-source-name
+	//}), &gorm.Config{})
 
 	if err != nil {
 		fmt.Printf("err:%s\n", err)
@@ -25,7 +42,25 @@ func main() {
 		UpdateTime: 2000,
 	}
 
-	db.Create(dataUser)
+	err = db.Create(dataUser).Error
+
+	if err != nil {
+		fmt.Printf("Create err:%s\n", err)
+		return
+	}
+
+	dataUser2 := &entity.DataUser{
+		Name:       "test2",
+		CreateTime: 1000,
+		UpdateTime: 2000,
+	}
+
+	err = db2.Create(dataUser2).Error
+
+	if err != nil {
+		fmt.Printf("Create2 err:%s\n", err)
+		return
+	}
 
 	fmt.Printf("%+v \n", dataUser)
 }
